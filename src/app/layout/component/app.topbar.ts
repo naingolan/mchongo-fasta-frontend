05e7f70ffba40db447de30f7973e52c1cacd2cf6
naingolan <kelvinsdechaw@gmail.com>
2026-09-01 09:53:11 +0300

    Remove template theme configurator widget and palette menu from topbar

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -3,13 +3,12 @@ import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
-import { AppConfigurator } from './app.configurator';
 import { LayoutService } from '../service/layout.service';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator],
+  imports: [RouterModule, CommonModule, StyleClassModule],
   template: `
     <div class="layout-topbar">
       <div class="layout-topbar-logo-container">
@@ -29,22 +28,6 @@ import { LayoutService } from '../service/layout.service';
           <button type="button" class="layout-topbar-action" (click)="toggleDarkMode()" title="Dark/Light Mode">
             <i [ngClass]="{ 'pi ': true, 'pi-moon': layoutService.isDarkTheme(), 'pi-sun': !layoutService.isDarkTheme() }"></i>
           </button>
-
-          <div class="relative">
-            <button
-              class="layout-topbar-action layout-topbar-action-highlight"
-              pStyleClass="@next"
-              enterFromClass="hidden"
-              enterActiveClass="animate-scalein"
-              leaveToClass="hidden"
-              leaveActiveClass="animate-fadeout"
-              [hideOnOutsideClick]="true"
-              title="Theme Settings"
-            >
-              <i class="pi pi-palette"></i>
-            </button>
-            <app-configurator />
-          </div>
         </div>
 
         <div class="layout-topbar-user">
